ed107951ca58b225b678003b1f37997d9a614bbd
Idiane05 <d.ingabire1@alustudent.com>
2023-07-09 11:00:51 -0700

    Updated task

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def concat_strings(string1, string2):
-"""Concatenates two strings and removes trailing whitespace."""
-str1 = str1.lstrip()
-str2 = str2.lstrip()
-return str1 + " " + str2
-print(concat_strings("Holberton", "School"))
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print("Welcome to {}!".format(str1))  
